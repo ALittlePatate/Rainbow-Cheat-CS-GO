0bc18a98da0f723634458c82f382cc4d8f25d1f9
ALittlePatate <48603993+ALittlePatate@users.noreply.github.com>
2020-09-23 15:48:10 +0200

    Update rcs.py

--- utils/rcs.py
+++ utils/rcs.py
@@ -55,6 +55,27 @@ def main():
     client = pymem.process.module_from_name(pm.process_handle, "client.dll").lpBaseOfDll
     engine = pymem.process.module_from_name(pm.process_handle, "engine.dll").lpBaseOfDll
 
+    #On cherche le nom de la config actuelle
+    with open("configs/path.txt") as path_txt :
+        path = path_txt.readline()
+        path_txt.close()
+
+    #On ouvre la config et on en tire la variable RCSPerfectPercent
+    with open("configs/"+path) as config_file :
+        line_nmb = 1
+        for line in config_file :
+            if line_nmb == 15 :
+                RCSPerfectPercent = line
+            if line_nmb >= 16 :
+                config_file.close()
+                break
+            line_nmb = line_nmb + 1
+    
+    RCSPerfectPercent = RCSPerfectPercent.replace("\n", "")
+    RCSPerfectPercent = RCSPerfectPercent.replace(" ", "")
+    RCSPerfectPercent = float(RCSPerfectPercent)
+
+
     global amount
     oldpunchx = 0.0
     oldpunchy = 0.0
@@ -68,8 +89,8 @@ def main():
                 rcs_y = pm.read_float(rcsengine + dwClientState_ViewAngles + 0x4)
                 punchx = pm.read_float(rcslocalplayer + m_aimPunchAngle)
                 punchy = pm.read_float(rcslocalplayer + m_aimPunchAngle + 0x4)
-                newrcsx = rcs_x - (punchx - oldpunchx) * 2
-                newrcsy = rcs_y - (punchy - oldpunchy) * 2
+                newrcsx = rcs_x - (punchx - oldpunchx) * (RCSPerfectPercent * 0.02)
+                newrcsy = rcs_y - (punchy - oldpunchy) * (RCSPerfectPercent * 0.02)
                 newrcs, newrcy = normalizeAngles(newrcsx, newrcsy)
                 oldpunchx = punchx
                 oldpunchy = punchy
@@ -81,6 +102,8 @@ def main():
                 oldpunchy = 0.0
                 newrcsx = 0.0
                 newrcsy = 0.0
+            if keyboard.is_pressed('delete'):
+                _exit(0)
 
 
 if __name__ == '__main__':
