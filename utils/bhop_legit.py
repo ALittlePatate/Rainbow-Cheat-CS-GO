e45f91c2001e34343d35bb7b6cb1c296d6c8e33e
ALittlePatate <48603993+ALittlePatate@users.noreply.github.com>
2020-10-25 16:48:42 +0100

    Add files via upload

---
+++ utils/bhop_legit.py
@@ -0,0 +1,41 @@
+import keyboard
+import pymem
+import pymem.process
+import time
+import ctypes
+import warnings
+from random import *
+warnings.simplefilter("ignore")
+from Offsets import *
+from win32gui import GetWindowText, GetForegroundWindow
+
+def main():
+    try :
+        pm = pymem.Pymem("csgo.exe")
+    except :
+        MessageBox = ctypes.windll.user32.MessageBoxW
+        MessageBox(None, 'Could not find the csgo.exe process !', 'Error', 16)
+        return
+    client = pymem.process.module_from_name(pm.process_handle, "client.dll").lpBaseOfDll
+
+    while True:
+        if not GetWindowText(GetForegroundWindow()) == "Counter-Strike: Global Offensive":
+            continue
+
+        if keyboard.is_pressed("space"):
+            force_jump = client + dwForceJump
+            player = pm.read_int(client + dwLocalPlayer)
+            if player:
+                on_ground = pm.read_int(player + m_fFlags)
+                if on_ground and on_ground == 257:
+                    a = randint(0,5)
+                    time.sleep(a/100)
+                    pm.write_int(force_jump, 5)
+                    time.sleep(0.08)
+                    pm.write_int(force_jump, 4)
+
+        time.sleep(0.002)
+
+
+if __name__ == '__main__':
+    main()
